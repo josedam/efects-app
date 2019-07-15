a6b30befd3643c6c2938d695ce23fe11e4819dc1
josedam <josedam@yahoo.com>
2019-07-14 22:47:24 -0300

    Primer commit

---
+++ src/app/usuarios/lista/lista.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../../services/usuario.service';
+import { Usuario } from 'src/app/models/usuario.model';
+
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html',
+  styles: []
+})
+export class ListaComponent implements OnInit {
+  usuarios: Usuario[] = [];
+
+  constructor( private usuarioService: UsuarioService) { }
+
+  ngOnInit() {
+    this.usuarioService.getUsers()
+      .subscribe((data: any) => {
+        this.usuarios = data;
+        console.log(data);
+      } );
+  }
+
+}
